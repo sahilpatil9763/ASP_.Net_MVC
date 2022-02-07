c0a6ddf595e1ee90b8f3494be3d2bccfcd27158a
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2022-02-07 18:47:08 +0530

    Update Modal.cs

--- Bike_Selling_Management_System/Bike_Selling_Management_System/Models/Modal.cs
+++ Bike_Selling_Management_System/Bike_Selling_Management_System/Models/Modal.cs
@@ -7,6 +7,11 @@ namespace Bike_Selling_Management_System.Models
 {
     public class Modal
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeID { get; set; }
     }
 }
