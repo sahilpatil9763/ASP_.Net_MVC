2e2f848733d8a8b7e8897a09c62363496fab10ea
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2022-02-26 19:58:24 +0530

    Update

---
+++ Bulky_Book/Bulky_Book/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bulky_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
